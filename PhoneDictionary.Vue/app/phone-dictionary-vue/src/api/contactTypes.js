c6d2f6cfc4d82966e9bc14e445ec2b2788d0cae5
Nikita Dermenzhi <nikita.dermenzhi@gmail.com>
2021-03-12 00:52:38 +0200

    Update contactTypes.js

--- PhoneDictionary.Vue/app/phone-dictionary-vue/src/api/contactTypes.js
+++ PhoneDictionary.Vue/app/phone-dictionary-vue/src/api/contactTypes.js
@@ -2,7 +2,9 @@ const axios = require('axios')
 
 export default {
   async getAllContactTypes () {
-    var contactTypes = await axios.get(`${process.env.VUE_APP_API}/ContactTypes`)
+    var contactTypes = await axios.get(
+      `${process.env.VUE_APP_API}/ContactTypes`
+    )
     return contactTypes
   }
 }
